7f875d43917c7512d11d805780ccc4f21878542b
Paulo Campos <paulo4-0@hotmail.com>
2024-08-04 22:33:32 -0300

    chore: change pathfile of start app from module to shared

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import { userRouter } from "../../../modules/users/interfaces/http/userRoutes";
+import { config } from "../../config/config";
+
+const origin = {
+	origin: "*",
+};
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(origin));
+app.use(helmet());
+
+const v1Router = express.Router();
+
+v1Router.get("/", (req, res) => {
+	return res.json({ message: "Yo! we're up" });
+});
+
+v1Router.use("/users", userRouter);
+
+app.use("/api/v1", v1Router);
+
+app.listen(config.PORT, () => {
+	console.log(`[App]: Listening on port ${config.PORT}`);
+});
